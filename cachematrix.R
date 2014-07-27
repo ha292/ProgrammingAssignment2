f5ffcc38b483c57dca7c86727241d51cbbb533af
Hafiz Ahsan <hafizahsan@gmail.com>
2014-07-27 01:36:04 -0400

    Assignment 3 first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions here -- makeCacheMatrix() and cacheSolve. At a high level, the work
+## together to cache the inverse of matrices so that we can save repeated calls to solve(). 
+## The idea is to save/cache the results of the calls to solve() functions because computing 
+## inverse is an expensive operation
 
-## Write a short comment describing this function
 
+## makeCahce() wraps a matrix into a data structure that also stores its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    m <-NULL
+    
+    ## setter for the wrapped object. If we set the matrix, we need to forget the old inverse
+    set <- function(y) {
+        ## "superassignment" searches upward in the lexical scope. 
+        x <<- y
+        m <<- NULL
+    }
+    
+    ## getter for the wrapped matrix 
+    get <- function() x
+    
+    ## sets the inverse
+    setinverse <- function(inverse) m <<-inverse
+    
+    ## returns the stored inverse
+    getinverse <- function() m
+    
+    ## return value for makeCache -- a list of functions
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns cached matrix values
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x' ca
+    
+    ## try to get the inverse
+    m <- x$getinverse()
+    
+    ## if inverse isn't present
+    if (is.null(m)) {
+        message("cache miss - computing and storing ")
+        
+        ## get the inner matrix
+        data <- x$get()
+        ## calculate the inverse
+        m <- solve(data, ...)
+        ## store the result
+        x$setinverse(m)
+        return (m)
+    } else {
+        message("cache hit")
+        return (m)
+    }
 }
